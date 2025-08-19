3ddaffddf7c43bf8f92151235d348a24b8597002
Nivetha K <thaniva938@gmail.com>
2025-08-19 19:42:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+// src/App.js
+import React from "react";
+import Home from "./components/Home";   // ✅ corrected path
+import "./App.css";
+
+function App() {
+  return <Home />;
+  
+}
+
+export default App;
